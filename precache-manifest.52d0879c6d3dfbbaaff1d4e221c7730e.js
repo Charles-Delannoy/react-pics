47092956fb49c2144827f6ff328a3b224df84c3c
Charles Delannoy <charles.delannoy@hei.fr>
2020-09-16 16:20:00 +0200

    Updates

---
+++ precache-manifest.52d0879c6d3dfbbaaff1d4e221c7730e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f5126d900f500f58701ea24d11de20",
+    "url": "/react-pics/index.html"
+  },
+  {
+    "revision": "da63f9a37d573f474110",
+    "url": "/react-pics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3aa780c8d3cf7d200371",
+    "url": "/react-pics/static/js/2.e64f08cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pics/static/js/2.e64f08cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da63f9a37d573f474110",
+    "url": "/react-pics/static/js/main.2b8a4aba.chunk.js"
+  },
+  {
+    "revision": "89367c4f6216cb127de6",
+    "url": "/react-pics/static/js/runtime-main.3a62fe00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pics/static/media/logo.5d5d9eef.svg"
+  }
+]);
